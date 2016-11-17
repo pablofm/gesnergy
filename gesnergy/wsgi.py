455e5d1f53f3f6151130eec8c87c6c69ec74ea4b
Pablo FM <pafloma@gmail.com>
2016-11-17 14:57:03 +0100

    To heroku 2

--- gesnergy/wsgi.py
+++ gesnergy/wsgi.py
@@ -4,6 +4,7 @@ from whitenoise.django import DjangoWhiteNoise
 from configurations.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cuiquer.settings")
-
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
+    8  
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
